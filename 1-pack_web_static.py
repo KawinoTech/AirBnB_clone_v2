210a4d15b457f9fffe046d6b557ecc9df5d4f875
KawinoTech <techkawino@gmail.com>
2024-12-12 13:45:37 +0300

    fixed

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # Fabfile to generate a .tgz archive from the contents of web_static.
 
+import os
 from fabric.api import local
 from datetime import datetime
 
@@ -9,25 +10,25 @@ TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 def do_pack():
     """
-    Creates a tarball archive of the `web_static` directory and stores it in the 
-    `versions` folder. The archive is named with a timestamp.
+    Creates a tarball archive of the `web_static` directory and stores it
+    in the `versions` folder. The filename includes a timestamp for uniqueness.
 
     Returns:
-        str: The name of the created archive file, or None if an error occurred.
+        str: The name of the created archive, or None if an error occurs.
     """
-    local("mkdir versions")
-    
-    dt_object = datetime.strptime(
-        datetime.strftime(datetime.now(), TIME_FORMAT), TIME_FORMAT
-    )
-    
+    local("mkdir -p versions")  # Creates the directory if it doesn't exist
+
+    dt_object = datetime.strptime(datetime.strftime(datetime.now(),
+                                                    TIME_FORMAT), TIME_FORMAT)
     formatted_datetime = dt_object.strftime("%Y%m%d%H%M%S")
     filename = 'web_static_' + formatted_datetime
-    
+
     try:
         print(f"Packing web_static to versions/{filename}.tgz")
         local(f"tar -czvf versions/{filename}.tgz web_static")
-        print(f"web_static packed: versions/{filename}.tgz -> 21283Bytes")
+        print(f"web_static packed: versions/{filename}.tgz ->
+              {os.path.getsize(f'./versions/{filename}.tgz')} Bytes")
+
         return filename
     except (OSError, FileNotFoundError) as e:
         print(f"Error: {e}")
