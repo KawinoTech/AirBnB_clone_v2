fbb179570dc3865b0a3e2935f362599dba4a2f95
Jeremy <techkawino@gmail.com>
2024-12-23 07:23:38 +0000

    fixed

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+"""
+Simple Flask application serving a single endpoint.
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def home():
+    """
+    Handle the root route and return a greeting message.
+    """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
